bcfc4995a708f76410e015df3c52c22ac73eb416
jackspades <galesme@yahoo.com>
2024-08-17 20:57:40 -0500

    ..

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "test_server" {
     ami = "ami-0c8e23f950c7725b9"
     instance_type = "t2.micro"
-    security_groups = "sg-06b956a6e7be23bf9"
+    vpc_security_group_ids = ["sg-06b956a6e7be23bf9"]
     tags = {
         Name = "Test Server"
     }
